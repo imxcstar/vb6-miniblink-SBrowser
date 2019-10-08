d6450053940a9bad7025d44c340ad241105fae23
imxcstar <1454643841@qq.com>
2019-10-08 16:48:15 +0800

    修改一些BUG

---
+++ Build/注册控件.bat
@@ -0,0 +1,13 @@
+@echo off
+@set root=%~dp0
+if %PROCESSOR_ARCHITECTURE% == AMD64 (
+if exist "%windir%\Syswow64\SBrowser_G_200.ocx" goto ends
+xcopy "%root%SBrowser_G_200.ocx" "%windir%\Syswow64\" /s/y
+xcopy "%root%node.dll" "%windir%\Syswow64\" /s/y
+regsvr32 %windir%\Syswow64\SBrowser_G_200.ocx /s
+) else (
+if exist "%windir%\System32\SBrowser_G_200.ocx" goto ends
+xcopy "%root%SBrowser_G_200.ocx" "%windir%\System32\" /s/y
+xcopy "%root%node.dll" "%windir%\System32\" /s/y
+regsvr32 %windir%\System32\SBrowser_G_200.ocx /s)
+:ends
