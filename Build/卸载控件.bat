4dfba3cc56850a1abef231408a5aa04f2e59d489
imxcstar <1454643841@qq.com>
2022-07-08 01:04:17 +0800

    修复一些回调方法的乱码，给控件添加了几个事件

--- Build/卸载控件.bat
+++ Build/卸载控件.bat
@@ -1,13 +1,13 @@
 @echo off
 if %PROCESSOR_ARCHITECTURE% == AMD64 (
-if not exist "%windir%\Syswow64\SBrowser_G_202.ocx" goto ends
-regsvr32 "%windir%\Syswow64\SBrowser_G_202.ocx" /s /u
-del /f /s /q "%windir%\Syswow64\SBrowser_G_202.ocx"
+if not exist "%windir%\Syswow64\SBrowser_G_203.ocx" goto ends
+regsvr32 "%windir%\Syswow64\SBrowser_G_203.ocx" /s /u
+del /f /s /q "%windir%\Syswow64\SBrowser_G_203.ocx"
 del /f /s /q "%windir%\Syswow64\node.dll"
 ) else (
-if not exist "%windir%\System32\SBrowser_G_202.ocx" goto ends
-regsvr32 "%windir%\System32\SBrowser_G_202.ocx" /s /u
-del /f /s /q "%windir%\System32\SBrowser_G_202.ocx"
+if not exist "%windir%\System32\SBrowser_G_203.ocx" goto ends
+regsvr32 "%windir%\System32\SBrowser_G_203.ocx" /s /u
+del /f /s /q "%windir%\System32\SBrowser_G_203.ocx"
 del /f /s /q "%windir%\System32\node.dll"
 )
 :ends
